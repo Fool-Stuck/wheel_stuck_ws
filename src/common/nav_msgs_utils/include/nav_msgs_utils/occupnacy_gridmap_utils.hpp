af836f787f0a0dad8eecc13e94a397855fed492a
Autumn60 <harada.akiro@gmail.com>
2024-07-01 22:03:13 +0900

    create .hpp file
    Signed-off-by: Autumn60 <harada.akiro@gmail.com>

---
+++ src/common/nav_msgs_utils/include/nav_msgs_utils/occupnacy_gridmap_utils.hpp
@@ -0,0 +1,17 @@
+#ifndef NAV_MSGS_UTILS__OCCUPANCY_GRIDMAP_UTILS_HPP_
+#define NAV_MSGS_UTILS__OCCUPANCY_GRIDMAP_UTILS_HPP_
+
+#include <nav_msgs/msg/occupancy_grid.hpp>
+
+namespace nav_msgs_utils
+{
+using OccupancyGrid = nav_msgs::msg::OccupancyGrid;
+
+static class OccupancyGridmapUtils
+{
+    public:
+}  // class OccupancyGridmapUtils
+
+}  // namespace nav_msgs_utils
+
+#endif  // NAV_MSGS_UTILS__OCCUPANCY_GRIDMAP_UTILS_HPP_
