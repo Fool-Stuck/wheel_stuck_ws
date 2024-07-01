10c263f13334cd6821d5927b837fdeb76df2fafb
Autumn60 <harada.akiro@gmail.com>
2024-07-01 21:37:15 +0900

    create pkg files
    Signed-off-by: Autumn60 <harada.akiro@gmail.com>

---
+++ src/perception/costmap_generator/include/costmap_generator/costmap_generator.hpp
@@ -0,0 +1,13 @@
+#ifndef COSTMAP_GENERATOR__COSTMAP_GENERATOR_HPP_
+#define COSTMAP_GENERATOR__COSTMAP_GENERATOR_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace costmap_generator
+{
+class CostmapGenerator : public rclcpp::Node
+{
+};
+}  // namespace costmap_generator
+
+#endif  // COSTMAP_GENERATOR__COSTMAP_GENERATOR_HPP_
