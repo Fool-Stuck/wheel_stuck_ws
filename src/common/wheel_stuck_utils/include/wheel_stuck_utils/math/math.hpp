26f8c1b0d5833086e5f96bda2818f49fb8bc1b9a
Bey9434 <aluca9434@gmail.com>
2024-07-14 00:50:23 +0900

    add clamp function

--- src/common/wheel_stuck_utils/include/wheel_stuck_utils/math/math.hpp
+++ src/common/wheel_stuck_utils/include/wheel_stuck_utils/math/math.hpp
@@ -10,6 +10,12 @@ inline double lerp(const double a, const double b, double t)
   t = std::max(0.0, std::min(1.0, t));
   return a + t * (b - a);
 }
+
+inline double clamp(double value, double min, double max)
+{
+  return std::max(min, std::min(value, max));
+}
+
 }  // namespace wheel_stuck_utils::math
 
 #endif  // WHEEL_STUCK_UTILS__MATH__MATH_HPP_
