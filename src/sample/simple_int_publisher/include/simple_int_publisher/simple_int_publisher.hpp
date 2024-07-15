034885ed465c76bd2d370306e3185efced4eb0ed
Autumn60 <harada.akiro@gmail.com>
2024-07-15 18:16:28 +0900

    make simple_int_publisher to use TimerDrivenNode
    Signed-off-by: Autumn60 <harada.akiro@gmail.com>

--- src/sample/simple_int_publisher/include/simple_int_publisher/simple_int_publisher.hpp
+++ src/sample/simple_int_publisher/include/simple_int_publisher/simple_int_publisher.hpp
@@ -2,24 +2,29 @@
 #define SIMPLE_INT_PUBLISHER__SIMPLE_INT_PUBLISHER_HPP_
 
 #include <rclcpp/rclcpp.hpp>
+#include <wheel_stuck_utils/ros/timer_driven_node.hpp>
 
 #include <std_msgs/msg/int32.hpp>
 
 namespace simple_int_publisher
 {
 
+using TimerDrivenNode = wheel_stuck_utils::ros::TimerDrivenNode;
+
 using Int32 = std_msgs::msg::Int32;
 
 using Int32Publisher = rclcpp::Publisher<Int32>;
 
-class SimpleIntPublisher : public rclcpp::Node
+class SimpleIntPublisher : public TimerDrivenNode
 {
 public:
   explicit SimpleIntPublisher(const rclcpp::NodeOptions & options);
-  void update();
+
+protected:
+  void on_init() override;
+  void on_update() override;
 
 private:
-  rclcpp::TimerBase::SharedPtr update_timer_;
   Int32Publisher::SharedPtr pub_;
   int counter_;
 };
