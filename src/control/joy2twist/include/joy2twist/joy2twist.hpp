af26902f6d153742dd2439ba88f4cff5d087d4f3
Bey9434 <134945706+Bey9434@users.noreply.github.com>
2024-07-23 19:54:26 +0900

    Feat/control/joy2twist (#64)
    * create control and joy2twist
    
    * create control and joy2twist
    
    * create launch
    
    * fix cmakelist,cpp
    
    * fix Joymsg, main, config
    
    * Add config file and parameter support to joy2twist node
    
    * fix launch
    
    ---------
    
    Co-authored-by: Autumn60 <37181352+Autumn60@users.noreply.github.com>

---
+++ src/control/joy2twist/include/joy2twist/joy2twist.hpp
@@ -0,0 +1,30 @@
+#ifndef JOY2TWIST__JOY2TWIST_HPP_
+#define JOY2TWIST__JOY2TWIST_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <geometry_msgs/msg/twist.hpp>
+#include <sensor_msgs/msg/joy.hpp>
+
+namespace joy2twist
+{
+using Joy = sensor_msgs::msg::Joy;
+using Twist = geometry_msgs::msg::Twist;
+
+class Joy2Twist : public rclcpp::Node
+{
+public:
+  explicit Joy2Twist(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+private:
+  void joy_callback(const Joy::SharedPtr msg);
+  int linear_x_axis_;
+  int angular_z_axis_;
+
+  rclcpp::Subscription<Joy>::SharedPtr joy_sub_;
+  rclcpp::Publisher<Twist>::SharedPtr twist_pub_;
+};
+
+}  // namespace joy2twist
+
+#endif  // JOY2TWIST__JOY2TWIST_HPP_
---
+++ src/control/joy2twist/src/joy2twist.cpp
@@ -0,0 +1,39 @@
+#include <joy2twist/joy2twist.hpp>
+
+namespace joy2twist
+{
+
+Joy2Twist::Joy2Twist(const rclcpp::NodeOptions & options) : Node("joy2twist", options)
+{
+  // パラメータの宣言と初期値の設定
+  declare_parameter<int>("linear_x_axis", 1);
+  declare_parameter<int>("angular_z_axis", 0);
+
+  // パラメータの取得
+  get_parameter("linear_x_axis", linear_x_axis_);
+  get_parameter("angular_z_axis", angular_z_axis_);
+
+  // 受信機を作る。型はjoyで受け取る。10個まで値が保持される。
+  joy_sub_ = this->create_subscription<Joy>(
+    "~/input/joy", 10, std::bind(&Joy2Twist::joy_callback, this, std::placeholders::_1));
+  // 送信機を作る。
+  twist_pub_ = this->create_publisher<Twist>("~/output/cmd_vel", 10);
+}
+
+void Joy2Twist::joy_callback(const Joy::SharedPtr msg)
+{
+  auto twist = Twist();
+
+  float linear_x = msg->axes[linear_x_axis_];    // 前後方向の入力
+  float angular_z = msg->axes[angular_z_axis_];  // 左右方向の入力
+
+  twist.linear.x = linear_x;
+  twist.angular.z = angular_z;
+
+  twist_pub_->publish(twist);
+}
+
+}  // namespace joy2twist
+#include "rclcpp_components/register_node_macro.hpp"
+
+RCLCPP_COMPONENTS_REGISTER_NODE(joy2twist::Joy2Twist)
